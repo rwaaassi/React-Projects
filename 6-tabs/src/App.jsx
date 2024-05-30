c2163f2bb6b6de61d1ce4f3c6b11c767db93c597
Rwaa Assi <rwaaassi123@gmail.com>
2024-05-30 14:26:32 +0300

    tabs

---
+++ 6-tabs/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import BtnContainer from "./BtnContainer";
+import JobInfo from "./JobInfo";
+
+const url = "https://www.course-api.com/react-tabs-project";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  if (loading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+  return (
+    <section className="jobs-center">
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
+}
+
+export default App;
